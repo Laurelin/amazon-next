fedbbcaa02caca494ccfa864139e3d5cbfc68758
Laurelin <ladylaurel18@gmail.com>
2021-07-01 17:40:09 -0400

    firebase bug fix

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -14,7 +14,7 @@ const serviceAccount = {
   type: 'service_account',
   project_id: 'next-ead37',
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: 'firebase-adminsdk-4vxzu@next-ead37.iam.gserviceaccount.com',
   client_id: '112153690685421676781',
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
@@ -24,11 +24,15 @@ const serviceAccount = {
     'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-4vxzu%40next-ead37.iam.gserviceaccount.com'
 };
 
+console.log('service account:', serviceAccount);
+
 const app = !admin.apps.length
   ? admin.initializeApp({
       credential: admin.credential.cert(serviceAccount)
     })
   : admin.app();
+
+console.log('app:', app);
 
 const fulfillOrder = async (session) => {
   console.log('Fulfilling order: ', session);
@@ -47,9 +51,10 @@ const fulfillOrder = async (session) => {
     .then(() => console.log(`SUCCESS: Order ${session.id} has been added to the DB`));
 };
 
-export default async (req, res) => {
+export default async function webhook(req, res) {
   if (req.method === 'POST') {
     // get signature from request(hopefully from stripe)
+    console.log('first webhook request:', req);
     const requestBuffer = await buffer(req);
     const payload = requestBuffer.toString();
     const sig = req.headers['stripe-signature'];
@@ -65,12 +70,13 @@ export default async (req, res) => {
 
     if (event.type === 'checkout.session.completed') {
       const session = event.data.object;
+      console.log('session completed event (session):', session);
       return fulfillOrder(session)
         .then(() => res.status(200))
         .catch((err) => res.status(400).send(`Webhook Error: ${err.message}`));
     }
   }
-};
+}
 
 export const config = {
   api: {
